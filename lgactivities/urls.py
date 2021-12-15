1a3cde630c1da975a37008dbda0ed05a21ad31a1
Luc Galarneau <lgalarno@gmail.com>
2021-12-15 00:18:38 -0500

    minor updates

--- lgactivities/urls.py
+++ lgactivities/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="main"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
+    # TODO about page
     path('about/', TemplateView.as_view(template_name="index.html"), name="about"),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('accounts/', include('allauth.urls')),
