2822b79a60ce8d9dd7276637b644605982d8d8ce
luc <lgalarno@gmail.com>
2022-07-29 02:54:15 -0400

    use one settings file; activities list - fixes wsgi 2

--- lgactivities/asgi.py
+++ lgactivities/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgactivities.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgactivities.settings')
 
 application = get_asgi_application()
