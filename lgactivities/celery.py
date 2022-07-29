04112e5102d03d2b10aeca999307b851ee9dd1c2
luc <lgalarno@gmail.com>
2022-07-29 03:54:49 -0400

    use one settings file; activities list - fixes celery

--- lgactivities/celery.py
+++ lgactivities/celery.py
@@ -2,10 +2,10 @@ import os
 from celery import Celery
 
 # set the default Django settings_old module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgactivities.settings_old')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgactivities.settings')
 
 app = Celery('lgactivities')
-app.config_from_object('django.conf:settings_old', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
